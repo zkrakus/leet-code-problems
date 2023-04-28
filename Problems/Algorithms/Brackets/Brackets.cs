f336e0243143c47b57cefe2adc42155ae3e3cc92
zkrakus <zkrakus@gmail.com>
2023-04-28 16:06:01 -0400

    Brackets

---
+++ Problems/Algorithms/Brackets/Brackets.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Algorithms.Brackets;
+
+/// <summary>
+/// A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+/// 
+/// S is empty;
+/// S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+/// S has the form "VW" where V and W are properly nested strings.
+/// 
+/// For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+/// 
+/// Given a string S consisting of N characters, return 1 if S is properly nested and 0 otherwise.
+/// </summary>
+public static class Brackets
+{
+    public static int Run(string s)
+    {
+        if (s == null || s.Length == 0)
+            return 1;
+
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> brackets = new Dictionary<char, char>() { { '{', '}' }, { '[', ']' }, { '(', ')' } };
+        foreach (var c in s)
+        {
+            if (brackets.ContainsKey(c))
+                stack.Push(c);
+            else
+            {
+                try 
+                {
+                    if (brackets[stack.Peek()] != c)
+                        return 0;
+                } catch { return 0; }
+                    
+                _ = stack.Pop();
+            }
+        }
+
+        return stack.IsNullOrEmpty() ?  1 : 0;
+    }
+
+
+}
---
+++ Tests/Problems/Algorithms/BracketsTests/RunTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Algorithms.Brackets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Problems.Algorithms.BracketsTests;
+public class RunTests
+{
+    [Theory]
+    [InlineData(1, "{[()()]}")]
+    [InlineData(0, "([)()]")]
+    [InlineData(1, "")]
+    [InlineData(1, null)]
+    [InlineData(0, "{")]
+
+    public void ShouldReturnSolution_ForInput(int assertion, string input)
+    {
+        Assert.Equal(assertion, Brackets.Run(input));
+    }
+}
